86f141e556e91a1a95fc62eb89a38c5364459c41
d546-star <d546-star@users.noreply.github.com>
2026-06-22 20:05:34 +0000

    deploy: 2910fe3168246a085f6c00714f271cd45906d868

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+let allData = [];
+
 fetch('bursaries.json')
   .then(res => res.json())
   .then(data => {
-    const container = document.getElementById("list");
-
-    data.forEach(item => {
-      const div = document.createElement("div");
-      div.className = "card";
-
-      div.innerHTML = `
-        <h3>${item.title}</h3>
-        <p>${item.description}</p>
-        <a href="${item.link}" target="_blank">Apply Here</a>
-      `;
-
-      container.appendChild(div);
-    });
+    allData = data;
+    render(data);
   });
+
+function render(data) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+
+  data.forEach(item => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <h3>${item.title}</h3>
+      <p>${item.description}</p>
+      <a href="${item.link}" target="_blank">Apply Here</a>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+function filterBursaries() {
+  const q = document.getElementById("search").value.toLowerCase();
+
+  const filtered = allData.filter(b =>
+    b.title.toLowerCase().includes(q) ||
+    b.description.toLowerCase().includes(q)
+  );
+
+  render(filtered);
+}
