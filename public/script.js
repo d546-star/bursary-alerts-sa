ccee1294ff212196053773dffef64547a48da296
d546-star <dumso546@gmail.com>
2026-06-22 23:19:56 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -11,3 +11,14 @@ fetch('bursaries.json')
       </div>
     `).join("");
   });
+fetch('bursaries.json')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("list").innerHTML = data.map(item => `
+      <div class="card">
+        <h3>${item.title}</h3>
+        <p>${item.description}</p>
+        <a href="bursary/${item.slug}.html">View Full Details</a>
+      </div>
+    `).join("");
+  });
